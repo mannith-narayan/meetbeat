a458a2a49f16e9f91bc7dd7435dfc2708a32671b
mannith-narayan <mannithnarayan03@gmail.com>
2024-01-22 06:32:59 +1100

    Storage and database migration- Completed

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use App\Models\Meeting;
 
 class MeetingController extends Controller
 {
@@ -39,13 +40,30 @@ class MeetingController extends Controller
             'audio-file.mimes' => 'The audio file must be a mp3, mp4, m4a or wav file.',
             'audio-file.max' => 'The audio file may not be greater than 100MB.',
           ];
-
+        
+        // validate the request
         $validatedData = $request->validate([
             'meeting-title' => 'required|max:256',
             'meeting-desc' => 'required|max:1024',
             'audio-file' => 'required|file|mimes:mp3,mp4,m4a,wav|max:102400',
           ], $messages);
-          dd($validatedData);
+          
+        // store the meeting
+        $audioFile = $request->file('audio-file');
+
+        //create a unique name for the file to not overwrite any existing file
+        $fileName = uniqid() . '.' . $audioFile->getClientOriginalExtension();
+
+        //store the file
+        $audioFile->storeAs('public/audio', $fileName);
+
+        //create the meeting
+        Meeting::create([
+            'title' => $validatedData['meeting-title'],
+            'description' => $validatedData['meeting-desc'],
+            'audio_file' => $fileName,
+        ]);
+
     }
 
     /**
